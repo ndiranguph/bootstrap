daef93d2ff9ca39df86ec924eb4f82d8d8d38100
ndiranguph <philip.gakuo@strathmore.edu>
2024-10-02 14:40:08 +0300

    changes

--- contents/headings.php
+++ contents/headings.php
@@ -4,7 +4,7 @@ class headings{
         ?>
       <div class="p-5 mb-4 bg-body-tertiary rounded-3">
         <div class="container-fluid py-2">
-          <h1 class="display-5 fw-bold">Sign Up Form</h1>
+          <h1 class="display-5 fw-bold">Sign Up Form Assignment</h1>
           <p class="col-md-12 fs-6">This is an individual assignment. We assume that you already know how to set up a localhost server(s): & MySQL database and Apache web servers.<br>If not, your instructor will assist you to set it up.</p>
           <button class="btn btn-primary btn-lg" type="button">Example button</button>
         </div>
